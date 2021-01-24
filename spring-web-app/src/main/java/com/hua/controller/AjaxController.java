a58d0a8cf11ba252e936e4c67a4d1cdcbd5f9676
Juminiy <2285151098@qq.com>
2021-01-24 14:36:14 +0800

    ajax request is ok !

--- spring-web-app/src/main/java/com/hua/controller/AjaxController.java
+++ spring-web-app/src/main/java/com/hua/controller/AjaxController.java
@@ -1,6 +1,8 @@
 package com.hua.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +12,7 @@ import java.io.IOException;
 @RestController
 public class AjaxController {
     @RequestMapping(value = "/postQicqEmail",produces = "application/json;charset=UTF-8")
-    public void  postQicqEmail(String QicqNumber, HttpServletResponse response) throws IOException {
+    public void  postQicqEmail(@RequestBody String QicqNumber, HttpServletResponse response) throws IOException {
         response.setContentType("html/text");
         response.setCharacterEncoding("UTF-8");
         if(QicqNumber != null ){
